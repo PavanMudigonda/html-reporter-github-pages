8a5140073b2b7a84d8e80ded93eccbd4b7648eac
Pavan Mudigonda <mnpawan@gmail.com>
2023-09-04 02:21:48 +0000

    updated

--- index-html-generator-descending-order.py
+++ index-html-generator-descending-order.py
@@ -54,7 +54,7 @@ def process_dir(top_dir, opts):
 <html>
 <head>
     <meta charset="utf-8">
-    <title>TR TEST REPORT</title>
+    <title>TEST REPORT</title>
     <link rel="favicon" type="image/x-icon" href="favicon.ico">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
